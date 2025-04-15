f6734a37add3ded0449c6fa9fb29bca1aa3bdf91
andrestobar05 <andrewtobar05@gmail.com>
2025-04-15 00:26:51 -0600

    Add HomePage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "@/components/navbar";
 import MyPetsPage from "@/features/myPets/pages/MyPetsPage";
 import ExploreLostPets from "@/features/exploreLostPets/pages/ExploreLostPetsPage";
+import HomePage from "./home";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/my-pets" element={<MyPetsPage />} />
         <Route path="/explore-lost-pets" element={<ExploreLostPets />} />
       </Routes>
