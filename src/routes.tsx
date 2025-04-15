53cf6ac56489ebf08fdad8153aa7786dd47b512b
andrestobar05 <andrewtobar05@gmail.com>
2025-04-14 23:18:24 -0600

    Add routes configuration for application navigation

---
+++ src/routes.tsx
@@ -0,0 +1,8 @@
+export const routes = {
+    home: "/",
+    myPets: "/my-pets",
+    exploreLostPets: "/explore-lost-pets",
+    signIn: "/sign-in",
+    logout: "/logout",
+    signUp: "/sign-up",
+  };
