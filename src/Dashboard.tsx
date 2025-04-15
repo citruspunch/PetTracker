c1f98fc450185d48deba57a7f8668dd03cb1193b
andrestobar05 <andrewtobar05@gmail.com>
2025-04-15 17:02:16 -0600

    Add Dashboard component with Navbar integration

---
+++ src/Dashboard.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>Dashboard</h1>
+            <p>Welcome to your dashboard!</p>
+        </div>
+    );
+}
+export default Dashboard;
