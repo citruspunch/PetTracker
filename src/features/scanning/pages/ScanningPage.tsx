2d86d1ad21c347982cfac2ddc26d69379cc37958
andrestobar05 <andrewtobar05@gmail.com>
2025-04-14 23:20:15 -0600

    Add ScanningPage component with initial structure

---
+++ src/features/scanning/pages/ScanningPage.tsx
@@ -0,0 +1,9 @@
+const ScanningPage = () => {
+    return (
+        <div>
+            <h1>Scanning Page</h1>
+            <p>This is the scanning page.</p>
+        </div>
+    );
+}
+export default ScanningPage; 
