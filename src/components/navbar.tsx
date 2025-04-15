45958f25b754dd9c12692fc1a63f5f0e9a18a657
andrestobar05 <andrewtobar05@gmail.com>
2025-04-15 00:27:12 -0600

    Update Navbar component to center container with mx-auto class

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -102,7 +102,7 @@ const Navbar = ({
 }: Navbar1Props) => {
     return (
         <section className="py-4">
-            <div className="container px-6">
+            <div className="container mx-auto">
                 {/* Desktop Menu */}
                 <nav className="hidden justify-between lg:flex">
                     <div className="flex items-center gap-6">
