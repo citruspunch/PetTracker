5768b5a5fbfa6aacb1d18c57c1e016293f5baab8
andrestobar05 <andrewtobar05@gmail.com>
2025-04-15 20:20:26 -0600

    Refactor routes to ensure proper organization and maintainability

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,17 @@
-import Dashboard from "./Dashboard";
+import { report } from "process";
 
 export const routes = {
     home: "/",
     dashboard: "/Dashboard",
     myPets: "/my-pets",
     exploreLostPets: "/explore-lost-pets",
+    reportLostPet: "/report-lost-pet",
+    reportFoundPet: "/report-found-pet",
+    petDetails: "/pet-details",
+    editPet: "/edit-pet",
+    editUser: "/edit-user",
+    resetPassword: "/reset-password",
     signIn: "/sign-in",
-    logout: "/logout",
     signUp: "/sign-up",
+    logout: "/logout",
   };
