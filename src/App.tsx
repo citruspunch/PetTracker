896a3d09490a03e5949d9967b8c0d78137114e02
andrestobar05 <andrewtobar05@gmail.com>
2025-04-15 20:20:43 -0600

    Refactor routing in App component to use routes constants for improved maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Dashboard from "./Dashboard";
 import { useState } from "react";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
+import { routes } from "./routes";
+import ResetPassword from "./auth/ResetPassword";
 
 function App() {
   // TODO - Implement authentication logic
@@ -14,12 +16,13 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Dashboard" element={user ? <Dashboard /> : <Login />} />
-        <Route path="/sign-in" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/my-pets" element={<MyPetsPage />} />
-        <Route path="/explore-lost-pets" element={<ExploreLostPets />} />
+        <Route path={routes.home} element={<HomePage />} />
+        <Route path={routes.dashboard} element={user ? <Dashboard /> : <Login />} />
+        <Route path={routes.signIn} element={<Login />} />
+        <Route path={routes.signUp} element={<Signup />} />
+        <Route path={routes.resetPassword} element={<ResetPassword />} />
+        <Route path={routes.myPets} element={<MyPetsPage />} />
+        <Route path={routes.exploreLostPets} element={<ExploreLostPets />} />
       </Routes>
     </Router>
   );
