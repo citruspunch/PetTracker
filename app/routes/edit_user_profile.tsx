68086137c635c71111d5525975b8ad6914fbd8b8
andrestobar05 <andrewtobar05@gmail.com>
2025-04-25 12:49:50 -0600

    fix: enforce required validation for last name in user profile schema and update edit user profile layout

--- app/routes/edit_user_profile.tsx
+++ app/routes/edit_user_profile.tsx
@@ -1,5 +1,13 @@
+import Navbar from '@/components/navbar'
 import CompleteProfileView from '@/features/profile/views/CompleteProfileView'
 
-const Component = () => <CompleteProfileView />
+const Component = () => (
+  <>
+  <Navbar />
+    <div className="flex flex-col items-center justify-center h-screen w-screen">
+      <CompleteProfileView />
+    </div>
+  </>
+)
 
 export default Component
