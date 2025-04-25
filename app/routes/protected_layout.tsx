2f5c6b01f562e86de25d4fd6e0d48fcb60b8bd3b
Pastel <pastel.dev.gt@gmail.com>
2025-04-25 08:08:26 -0600

    Add logging to protected layout client loader

--- app/routes/protected_layout.tsx
+++ app/routes/protected_layout.tsx
@@ -7,8 +7,20 @@ import type { Route } from './+types/protected_layout'
 // eslint-disable-next-line react-refresh/only-export-components
 export const clientLoader = async ({ request }: Route.ClientLoaderArgs) => {
   const userResult = await supabase.auth.getUser()
-  if (request.url.includes(appRoutes.petDetails)) return userResult.data.user
-  if (userResult.error) return redirect(appRoutes.login)
+  console.log('Obtained user')
+  console.log(userResult)
+
+  if (request.url.includes(appRoutes.petDetails)) {
+    console.log('Route matches exception')
+    console.log(request.url)
+    return userResult.data.user
+  }
+  if (userResult.error) {
+    console.log('Redirecting to login')
+    return redirect(appRoutes.login)
+  }
+
+  console.log('Navigation allowed')
   return userResult.data.user
 }
 
