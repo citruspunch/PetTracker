2f51393fe77034c6f7bf5b96d04c288800da5066
andrestobar05 <andrewtobar05@gmail.com>
2025-04-28 00:48:26 -0600

    Add WordReveal component to enhance heading animation in LandingView

--- app/features/landing/LandingView.tsx
+++ app/features/landing/LandingView.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { appRoutes } from '@/routes'
 import { Link } from 'react-router'
 import Navbar from '../../components/navbar'
+import WordReveal from '@/components/ui/word-reveal'
 
 interface Testimonial {
   quote: string
@@ -67,14 +68,16 @@ const LandingView = ({
   return (
     <>
       <Navbar hideMenu />
-      <section className="py-8">
+      <section className="py-4">
         <div className="container mx-auto px-6 lg:px-0">
           <div className="flex flex-col items-center gap-8 md:flex-row">
             <div className="flex-1">
               <div className="flex flex-col gap-4 lg:gap-8">
-                <h1 className="max-w-[80%] text-5xl leading-none font-bold text-foreground lg:text-5xl xl:text-7xl">
-                  {heading}
-                </h1>
+                <WordReveal
+                  text={heading}
+                  delay={0.4}
+                  className="max-w-[80%] text-5xl text-left leading-none font-bold text-foreground lg:text-5xl xl:text-7xl"
+                />
                 <p className="text-lg leading-snug text-muted-foreground xl:text-2xl">
                   {description}
                 </p>
